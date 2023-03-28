32bf98ce6404bf9858db12f1ada007a9fe99cd60
NICKY-TECH <tinaonyis@gmail.com>
2023-03-28 17:12:21 +0100

    add the index.js file

---
+++ index.js
@@ -0,0 +1,4 @@
+let name='ONYIBE CHUKWUAZONIM JUSTINA';
+let preferredProgrammingLanguage='Javascript';
+let vision=' I see myself making waves in the software and tech industry, solving problems faced by humans in this current day and age';
+console.log('my name is '+''+ name +' and my preferred programming language is '+ preferredProgrammingLanguage +' in the next one year '+ vision)
